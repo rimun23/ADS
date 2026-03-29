7602115ce6a5223667b7ab5d01689066fdd6bfd3
rimun <ingamesy@gmail.com>
2026-03-29 21:58:49 +0500

    task 5

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,20 @@
 import java.util.*;
 import java.lang.*;
 public class Main {
-    public static long power(int a, int number) {
+    public static int fibonacci(int number) {
         if (number == 0) {
+            return 0;
+        }
+        if (number == 1) {
             return 1;
         }
-        return a * power(a, number - 1);
+        return fibonacci(number - 1) + fibonacci(number - 2);
     }
     public static void main() {
         Scanner input = new Scanner(System.in);
-        System.out.print("Enter a number for power: ");
-        int a = input.nextInt();
+        System.out.print("Enter a number for factorial: ");
         int number = input.nextInt();
-        System.out.println(power(a, number));
+        System.out.println(fibonacci(number));
     }
 }
 
