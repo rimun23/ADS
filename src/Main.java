b32ab0978ee1f4a09fcf4239a90e824c94cd97ac
rimun <ingamesy@gmail.com>
2026-03-29 21:57:02 +0500

    task 7

--- src/Main.java
+++ src/Main.java
@@ -1,19 +1,24 @@
 import java.util.*;
 import java.lang.*;
 public class Main {
-    public static String isDigits(String s) {
-        for (int i = 0; i < s.length(); i++) {
-            if (!Character.isDigit(s.charAt(i))) {
-                return "No";
-            }
+    public static void permute(String string, String space) {
+        if (string.length() == 0) {
+            System.out.println(space);
+            return;
         }
-        return "Yes";
+
+        for (int i = 0; i < string.length(); i++) {
+            char ch = string.charAt(i);
+            String rest = string.substring(0, i) + string.substring(i + 1);
+            permute(rest, space + ch);
+        }
     }
     public static void main() {
         Scanner input = new Scanner(System.in);
-        System.out.print("Enter a number or text: ");
+        System.out.print("Enter a number for power: ");
         String string = input.next();
-        System.out.println(isDigits(string));
+        permute(string, "");
     }
 }
+
 
