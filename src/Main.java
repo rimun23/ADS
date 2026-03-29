f3427e80e4c2e1d30ca8e1ffb4b7b004d41891e9
rimun <ingamesy@gmail.com>
2026-03-29 21:59:25 +0500

    task 4

--- src/Main.java
+++ src/Main.java
@@ -1,21 +1,21 @@
 import java.util.*;
 import java.lang.*;
 public class Main {
-    public static int fibonacci(int number) {
-        if (number == 0) {
-            return 0;
-        }
-        if (number == 1) {
+    public static long factorial(int number){
+        if(number == 0 || number == 1){
             return 1;
         }
-        return fibonacci(number - 1) + fibonacci(number - 2);
+        else{
+            return number * factorial(number - 1);
+        }
     }
     public static void main() {
         Scanner input = new Scanner(System.in);
         System.out.print("Enter a number for factorial: ");
         int number = input.nextInt();
-        System.out.println(fibonacci(number));
+        System.out.println(factorial(number));
     }
 }
+
 
 
